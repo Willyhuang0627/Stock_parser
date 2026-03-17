1f827d5a53667d8135e975e16a44c21ed60e6c23
willyhuang <aa0981273968@gmail.com>
2026-03-17 14:51:51 +0800

    second update

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ password = "Aa12345678"
 #print("Import success")
 connect = pymssql.connect(server,user,password,database)
 print("Login success")
+
+cursor = connect.cursor()
+print("cursor取得成功")
+
+cursor.execute("select * from stocks")
